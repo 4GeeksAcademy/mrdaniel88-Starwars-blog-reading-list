bb48bcc1b18c3ff4857f10ec8f1f2da75a875b88
Daniel Martinez <d.martinez74@gmail.com>
2023-04-25 02:49:19 +0000

    navbar added

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,15 +3,36 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="container-fluid">
+					<a class="navbar-brand" href="#"><i class="fa-solid fa-jedi"></i></a>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="container-fluid collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+						
+						<ul class="navbar-nav mb-2 mb-lg-0">
+							<li class="nav-item dropdown">
+								<a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+									aria-expanded="false">
+									<button type="button" class="btn btn-primary d-flex gap-1">Favorites<i
+										class="fa-solid fa-caret-down"></i></button>
+								</a>
+								<ul class="dropdown-menu dropdown-menu-lg-end" aria-labelledby="navbarDropdown">
+									<li><a class="dropdown-item" href="#">Profile</a></li>
+									<li><a class="dropdown-item" href="#">Accesibility</a></li>
+									<li><a class="dropdown-item" href="#">Privacy and Data</a></li>
+									<li>
+										<hr class="dropdown-divider" />
+									</li>
+									<li><a class="dropdown-item text-danger" href="#">Log out</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
 	);
 };
