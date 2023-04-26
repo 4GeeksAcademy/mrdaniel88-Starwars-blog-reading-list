7205495c5d507ecb483497e46705120e3e5d87b0
Daniel Martinez <d.martinez74@gmail.com>
2023-04-26 15:57:35 +0000

    vehicles and planets cards added

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -19,20 +19,21 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			{/* {store.people && <Card/>} */}
+			<h1 className="title">Characters</h1>
 			<div className="row flex-nowrap overflow-auto mb-5">
 				{store.people &&
 					store.people.results.map(person => (
 						<PeopleCard key={person.name} person={person}/>
 					))}
 			</div>
-
+			<h1 className="title">Vehicles</h1>
 			<div className="row flex-nowrap overflow-auto mb-5">
 				{store.vehicles &&
 					store.vehicles.results.map(vehicle => (
 						<VehicleCard key={vehicle.name} vehicle={vehicle}/>
 					))}
 			</div>
-
+			<h1 className="title">Planets</h1>
 			<div className="row flex-nowrap overflow-auto mb-5">
 				{store.planets &&
 					store.planets.results.map(planet => (
