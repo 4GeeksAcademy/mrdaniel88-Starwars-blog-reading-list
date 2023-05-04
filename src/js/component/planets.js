e4dad984402807002d0c70d9cb3887072466cc77
Daniel Martinez <d.martinez74@gmail.com>
2023-05-04 17:27:45 +0000

    corrections in pagination lists

--- src/js/component/planets.js
+++ src/js/component/planets.js
@@ -18,7 +18,7 @@ export const Planets = () => {
         actions.fetchPages(elements).then((totalPages) => {
             setPages(totalPages);
         });
-    }, []);
+    }, [elements]);
    
 
     const updateList =(pageNumber) => {
